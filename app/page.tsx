2a3dc6f41dc6ad809a595be83657df3fb5794190
danielluis07 <bombdaniel@hotmail.com>
2024-03-27 20:44:00 -0300

    fixed window issue

--- app/page.tsx
+++ app/page.tsx
@@ -30,6 +30,14 @@ export default function Home() {
 
     window.addEventListener("resize", handleResize);
 
+    if (typeof window !== "undefined") {
+      window.addEventListener("resize", handleResize);
+
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }
+
     return () => {
       window.removeEventListener("resize", handleResize);
     };
